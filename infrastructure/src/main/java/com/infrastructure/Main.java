190eea6d3563a11e14ac281aa6427026bc7bc29d
kaliel_eduard <kalieleduard1@gmail.com>
2022-05-29 13:26:39 -0300

    Application properties

--- infrastructure/src/main/java/com/infrastructure/Main.java
+++ infrastructure/src/main/java/com/infrastructure/Main.java
@@ -3,10 +3,16 @@ package com.infrastructure;
 import com.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        config();
         SpringApplication.run(WebServerConfig.class, args);
+    }
+
+    private static void config() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development");
     }
 }
